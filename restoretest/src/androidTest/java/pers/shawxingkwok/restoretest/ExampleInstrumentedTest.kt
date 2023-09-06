7dcf93785d33c6aee671d664240e207a60f0b34f
WilliamKwokX <1425183716@qq.com>
2023-09-06 18:11:48 +0800

    +

--- restoretest/src/androidTest/java/pers/shawxingkwok/restoretest/ExampleInstrumentedTest.kt
+++ restoretest/src/androidTest/java/pers/shawxingkwok/restoretest/ExampleInstrumentedTest.kt
@@ -20,6 +20,5 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("pers.shawxingkwok.restoretest", appContext.packageName)
-        E().x++
     }
 }
