8010f2d4c1706fe542d820271390cf40f12c3d7f
WilliamKwokX <1425183716@qq.com>
2023-09-20 02:44:22 +0800

    Adapted mockk in tests

--- api/src/androidTest/java/pers/shawxingkwok/mvb/Observe.kt
+++ api/src/androidTest/java/pers/shawxingkwok/mvb/Observe.kt
@@ -1,10 +1,12 @@
-package pers.shawxingkwok.mvb
+﻿package pers.shawxingkwok.mvb
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.testing.launchFragment
 import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.mockk.mockkObject
+import io.mockk.verifyOrder
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import org.junit.Test
@@ -19,17 +21,20 @@ internal class Observe {
     }
 
     internal class MyFragment : Fragment() {
-        private val names = mutableListOf<String>()
-
-        private fun act(name: String){
-            names += name
-            assert(lifecycle.currentState == Lifecycle.State.STARTED)
+        companion object{
+            fun act(lifecycle: Lifecycle, name: String){
+                assert(lifecycle.currentState == Lifecycle.State.STARTED)
+            }
         }
 
-        private val x: Flow<Int> by rmb { flowOf(1) }.observe { act("x") }
-        private val y by saveMutableStateFlow { 1 }.observe { act("y") }
-        private val z by saveMutableSharedFlow<_, Int>(replay = 1).observe { act("z") }
-        private val a by saveMutableLiveData { 1 }.observe { act("a") }
+        init {
+            mockkObject(MyFragment)
+        }
+
+        private val x: Flow<Int> by rmb { flowOf(1) }.observe { act(lifecycle, "x") }
+        private val y by saveMutableStateFlow { 1 }.observe { act(lifecycle, "y") }
+        private val z by saveMutableSharedFlow<_, Int>(replay = 1).observe { act(lifecycle, "z") }
+        private val a by saveMutableLiveData { 1 }.observe { act(lifecycle, "a") }
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
@@ -38,8 +43,12 @@ internal class Observe {
 
         override fun onResume() {
             super.onResume()
-            assertAll(::x, ::y, ::z, ::a){
-                assert(it.name in names)
+            // verifyAll does not work in this case.
+            verifyOrder {
+                act(any(), "x")
+                act(any(), "y")
+                act(any(), "z")
+                act(any(), "a")
             }
         }
     }
